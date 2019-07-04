eae05657848b8ddfa254fdccb4a16879d1271645
Bruno Ribeiro <brunoenribeiro@gmail.com>
2019-07-04 07:58:23 -0300

    Implement type selection and random values with chance.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        objProps: ['id'],
+        objProps: [{ key: 'id', type: 'guid'}],
         amount: 1,
     },
     methods: {
         addProp() {
-            this.objProps.push('');
+            this.objProps.push({ key:'', type: '' });
         },
         removeProp(index) {
             this.objProps = this.objProps.filter((el, i) => i !== index);
@@ -18,8 +18,8 @@ var app = new Vue({
             for (i = 0; i < this.amount; i++) {
                 var obj = {};
                 this.objProps
-                    .filter(prop => prop)
-                    .forEach(prop => obj[prop] = chance.name());
+                    .filter(prop => prop.key && chance[prop.type])
+                    .forEach(prop => obj[prop.key] = chance[prop.type]());
                 json.push(obj);
             }
             return json;
